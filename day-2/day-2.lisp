7bb25bec9c7ff10d2e7bb9a7a655b59c4b367ca5
Emil Segerbäck <emilsegerback@gmail.com>
2017-12-02 12:09:11 +0100

    Solve part 2

--- day-2/day-2.lisp
+++ day-2/day-2.lisp
@@ -27,6 +27,20 @@
   (loop for (min max) in (mapcar #'min-and-max (get-nums))
 	 sum (- max min)))
 
+(defun possible-index-combos (n)
+  (loop for i from 0 below n
+	 append (loop for j from 0 below n
+			   when (not (eql i j))
+			   collect (list i j))))
 
+(defun division-combo (x y)
+  (when (eql (mod x y) 0)
+	(/ x y)))
 
+(defun possible-divisor (numbers)
+  (loop for (i j) in (possible-index-combos (length numbers))
+	 thereis (division-combo (elt numbers i) (elt numbers j))))
 
+(defun solve-part-2 ()
+  (loop for line in (get-nums)
+	 sum (possible-divisor line)))
