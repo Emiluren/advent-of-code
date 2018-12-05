f72502efa129844eb62891ab9e6a3972f3d92273
Emil Segerbäck <emilsegerback@gmail.com>
2018-12-05 10:19:36 +0100

    Make code faster

--- 2018/5/5.hs
+++ 2018/5/5.hs
@@ -1,23 +1,20 @@
 import Data.Char
 
-replace :: String -> String
-replace (c1:c2:cs) =
+-- The first string is the characters before the current point and the second
+-- are the ones after.
+replace :: String -> String -> String
+replace c1s [] = c1s
+replace [] (c2:c2s) = replace [c2] c2s
+replace (c1:c1s) (c2:c2s) =
   if isUpper c1 && isLower c2 && c1 == toUpper c2 then
-    replace cs
+    replace c1s c2s
   else if isLower c1 && isUpper c2 && c1 == toLower c2 then
-    replace cs
+    replace c1s c2s
   else
-    c1 : replace (c2 : cs)
-replace [] = []
-replace [c] = [c]
+    replace (c2:c1:c1s) c2s
 
 replaceUntilFinished :: String -> String
-replaceUntilFinished input =
-  let res = replace input
-  in if res == input then
-    res
-  else
-    replaceUntilFinished res
+replaceUntilFinished input = replace [] input
 
 partOne = length . replaceUntilFinished <$> readFile "5_input"
 
