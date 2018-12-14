adc2bee5f5ac9a49f8231a9e4001d80c2713dc0e
Emil Segerbäck <emilsegerback@gmail.com>
2018-12-14 17:36:33 +0100

    Add not quite working solution to day 14/12

---
+++ 2018/13.rs
@@ -0,0 +1,127 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+enum Turn { Left, Straight, Right }
+
+struct Cart {
+    pos: (usize, usize),
+    dir: char,
+    next_turn: Turn,
+}
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("13_input")?;
+    let mut input = String::new();
+    file.read_to_string(&mut input)?;
+    let input_lines: Vec<Vec<_>> = input.lines().map(|l| l.chars().collect()).collect();
+
+    // Find carts
+    let mut carts = Vec::new();
+    let mut y = 0;
+    let mut x = 0;
+    for line in input.lines() {
+        for c in line.chars() {
+            if c == 'v' || c == '^' || c == '<' || c == '>' {
+                carts.push(Cart { pos: (x, y), dir: c, next_turn: Turn::Left });
+            }
+            x += 1;
+        }
+        x = 0;
+        y += 1;
+    }
+
+    // Simulate until crash
+    'outer: loop {
+        carts.sort_by(|c1, c2| {
+            let (x1, y1) = c1.pos;
+            let (x2, y2) = c2.pos;
+
+            match y1.cmp(&y2) {
+                std::cmp::Ordering::Equal => {
+                    x1.cmp(&x2)
+                },
+                o => o
+            }
+        });
+
+        for i in 0..carts.len() {
+            let (x, y) = carts[i].pos;
+
+            let tile = input_lines[y as usize][x as usize];
+            if tile == '+' {
+                match carts[i].next_turn {
+                    Turn::Left => {
+                        carts[i].next_turn = Turn::Straight;
+                        carts[i].dir = match carts[i].dir {
+                            'v' => '>',
+                            '<' => 'v',
+                            '>' => '^',
+                            '^' => '<',
+                            _ => panic!("unknown dir {}", carts[i].dir)
+                        };
+                    },
+                    Turn::Straight => carts[i].next_turn = Turn::Right,
+                    Turn::Right => {
+                        carts[i].next_turn = Turn::Left;
+                        carts[i].dir = match carts[i].dir {
+                            'v' => '<',
+                            '<' => '^',
+                            '>' => 'v',
+                            '^' => '>',                            
+                            _ => panic!("unknown dir {}", carts[i].dir)
+                        };
+                    },
+                }
+            } else if tile == '/' {
+                carts[i].dir = match carts[i].dir {
+                    'v' => '<',
+                    '>' => '^',
+                    '<' => 'v',
+                    '^' => '>',
+                    _ => panic!("cart has illegal direction {} at / turn", carts[i].dir),
+                }
+            } else if tile == '\\' {
+                carts[i].dir = match carts[i].dir {
+                    '<' => '^',
+                    'v' => '>',
+                    '^' => '<',
+                    '>' => 'v',
+                    _ => panic!("cart has illegal direction {} at \\ turn", carts[i].dir),
+                }
+            }
+
+            carts[i].pos = match carts[i].dir {
+                'v' => (x, y+1),
+                '<' => (x-1, y),
+                '>' => (x+1, y),
+                '^' => (x, y-1),
+                _ => panic!("unknown dir {}", carts[i].dir)
+            };
+
+            for j in i..carts.len() {
+                let (x2, y2) = carts[j].pos;
+
+                if x == x2 && y == y2 {
+                    println!("First crash at {},{}", x, y);
+                    break 'outer;
+                }
+            }
+        }
+
+        // for y in input_lines.len() {
+        //     'line: for x in input_lines[y].len() {
+        //         for i in 0..carts.len() {
+        //             let (cx, cy) = carts[i].pos;
+        //             if cx == x && cy == y {
+        //                 print!("{}", carts[i].dir);
+        //                 continue 'line;
+        //             }
+        //         }
+        //         print!("{}", input_lines[y][x])
+        //     }
+        //     println!();
+        // }
+    }
+
+    Ok(())
+}
