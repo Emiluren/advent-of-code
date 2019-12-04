1abcad057df28d6cbedc1100f35c747cc00d27af
Emil Segerbäck <emil.segerback@attentec.se>
2019-12-04 08:42:36 +0100

    Solve 4b 2019

--- 2019/4.lisp
+++ 2019/4.lisp
@@ -10,10 +10,34 @@
       (or (and d1 d2 (= d1 d2))
           (two-adjacent-p (rest digs))))))
 
-(defun meets-criteria (digs)
+(defun meets-criteria-a (digs)
   (and (apply #'<= digs)
        (two-adjacent-p digs)))
 
-(defun solve-a ()
+(defun group-digits (digs current-group groups)
+  (if (not digs)
+      (if current-group
+          (cons current-group groups)
+          groups)
+      (if (and current-group (= (first digs) (first current-group)))
+          (group-digits (rest digs)
+                        (cons (first digs) current-group)
+                        groups)
+          (group-digits (rest digs)
+                        (list (first digs))
+                        (if current-group
+                            (cons current-group groups)
+                            groups)))))
+
+(defun meets-criteria-b (digs)
+  (and (apply #'<= digs)
+       (some (lambda (group)
+               (= (length group) 2))
+             (group-digits digs nil nil))))
+
+(defun solve ()
   (loop for i from 172851 upto 675869
-     count (meets-criteria (digits i nil))))
+     for digs = (digits i nil)
+     count (meets-criteria-a digs) into sol-a
+     count (meets-criteria-b digs) into sol-b
+     finally (return (values sol-a sol-b))))
