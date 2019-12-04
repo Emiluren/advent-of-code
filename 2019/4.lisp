acb69ae76387b4a8926cbed40e49473d659d1461
Emil Segerbäck <emil.segerback@attentec.se>
2019-12-04 14:03:13 +0100

    Refactor solution of 4/2019

--- 2019/4.lisp
+++ 2019/4.lisp
@@ -4,36 +4,23 @@
       (multiple-value-bind (quot rem) (floor num 10)
         (digits quot (cons rem digs)))))
 
-(defun two-adjacent-p (digs)
-  (when digs
-    (let ((d1 (first digs)) (d2 (nth 1 digs)))
-      (or (and d1 d2 (= d1 d2))
-          (two-adjacent-p (rest digs))))))
+(defun group-lengths (digs)
+  (if (endp digs)
+      nil
+      (let ((c (count (first digs) digs)))
+        (cons c (group-lengths (nthcdr c digs))))))
 
 (defun meets-criteria-a (digs)
   (and (apply #'<= digs)
-       (two-adjacent-p digs)))
-
-(defun group-digits (digs current-group groups)
-  (if (not digs)
-      (if current-group
-          (cons current-group groups)
-          groups)
-      (if (and current-group (= (first digs) (first current-group)))
-          (group-digits (rest digs)
-                        (cons (first digs) current-group)
-                        groups)
-          (group-digits (rest digs)
-                        (list (first digs))
-                        (if current-group
-                            (cons current-group groups)
-                            groups)))))
+       (some (lambda (group-len)
+               (>= group-len 2))
+             (group-lengths digs))))
 
 (defun meets-criteria-b (digs)
   (and (apply #'<= digs)
-       (some (lambda (group)
-               (= (length group) 2))
-             (group-digits digs nil nil))))
+       (some (lambda (group-len)
+               (= group-len 2))
+             (group-lengths digs))))
 
 (defun solve ()
   (loop for i from 172851 upto 675869
