546fbd73188497f8b307141aaab21af7cf803ef9
Emil Segerbäck <emil.segerback@attentec.se>
2019-12-11 11:16:00 +0100

    Solve 10a 2019

--- 2019/10.rs
+++ 2019/10.rs
@@ -25,31 +25,56 @@ fn main() {
         [0,0,1,1,0,0,0,1,1,1,0,0,1,1,1,1,1,1,1,1,1],
     ];
 
+    let width = input[0].len();
+    let height = input.len();
+
     let mut input_set = HashSet::new();
-    for row in 0..input.len() {
-        for col in 0..input[0].len() {
+    for row in 0..height {
+        for col in 0..width {
             if input[row][col] == 1 {
-                input_set.insert((row, col));
+                input_set.insert((row as isize, col as isize));
             }
         }
     }
 
     let mut max_asteroids = 0;
-    for row in 0..input.len() {
-        for col in 0..input[0].len() {
-            if !input[row][col] {
+    for (row, col) in &input_set {
+        let mut num_asteroids = 0;
+        for (r2, c2) in &input_set {
+            if (row, col) == (r2, c2) {
                 continue;
             }
+            let dy = r2 - row;
+            let dx = c2 - col;
+            let gcd = gcd(dx.abs() as usize, dy.abs() as usize) as isize;
 
-            let mut visible_asteroids = input.clone();
+            let (dx, dy) = (dx / gcd, dy / gcd);
 
-            for r in 0..input.len() {
-                for c in 0..input.len() {
-                    if r == row && c == col {
-                        continue;
-                    }
-                }
+            let mut check_row = row + dy;
+            let mut check_col = col + dx;
+
+            while !input_set.contains(&(check_row, check_col)) {
+                check_row += dy;
+                check_col += dx;
+            }
+
+            if (check_row, check_col) == (*r2, *c2) {
+                num_asteroids += 1;
             }
         }
+        max_asteroids = max_asteroids.max(num_asteroids);
     }
+
+    println!("Part 1: {}", max_asteroids);
+}
+
+fn gcd(x: usize, y: usize) -> usize {
+    let mut x = x;
+    let mut y = y;
+    while y != 0 {
+        let t = y;
+        y = x % y;
+        x = t;
+    }
+    x
 }
