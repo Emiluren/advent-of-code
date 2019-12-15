ad7279aebe32770f004578c161be1755f09190a8
Emil Segerbäck <emilsegerback@gmail.com>
2019-12-15 22:49:44 +0100

    Change 15/2019 to use backtracking

--- 2019/15.rs
+++ 2019/15.rs
@@ -8,6 +8,17 @@ use Tile::*;
 enum Dir { North = 1, South = 2, West = 3, East = 4 }
 use Dir::*;
 
+impl Dir {
+    fn reverse(&self) -> Dir {
+        match self {
+            North => South,
+            South => North,
+            West => East,
+            East => West,
+        }
+    }
+}
+
 fn main() {
     let original_input = vec![
         3,1033,1008,1033,1,1032,1005,1032,31,1008,1033,2,1032,1005,1032,58,1008,1033,3,1032,1005,1032,81,1008,1033,4,1032,1005,1032,104,99,1002,1034,1,1039,102,1,1036,1041,1001,1035,-1,1040,1008,1038,0,1043,102,-1,1043,1032,1,1037,1032,1042,1106,0,124,102,1,1034,1039,101,0,1036,1041,1001,1035,1,1040,1008,1038,0,1043,1,1037,1038,1042,1106,0,124,1001,1034,-1,1039,1008,1036,0,1041,1001,1035,0,1040,1002,1038,1,1043,101,0,1037,1042,1106,0,124,1001,1034,1,1039,1008,1036,0,1041,102,1,1035,1040,101,0,1038,1043,1001,1037,0,1042,1006,1039,217,1006,1040,217,1008,1039,40,1032,1005,1032,217,1008,1040,40,1032,1005,1032,217,1008,1039,7,1032,1006,1032,165,1008,1040,37,1032,1006,1032,165,1102,1,2,1044,1106,0,224,2,1041,1043,1032,1006,1032,179,1102,1,1,1044,1106,0,224,1,1041,1043,1032,1006,1032,217,1,1042,1043,1032,1001,1032,-1,1032,1002,1032,39,1032,1,1032,1039,1032,101,-1,1032,1032,101,252,1032,211,1007,0,39,1044,1105,1,224,1101,0,0,1044,1105,1,224,1006,1044,247,102,1,1039,1034,102,1,1040,1035,101,0,1041,1036,102,1,1043,1038,102,1,1042,1037,4,1044,1106,0,0,35,37,2,26,91,30,85,34,87,18,47,29,50,23,7,46,94,2,26,42,36,23,3,32,65,21,63,18,54,31,52,75,4,35,24,24,74,33,81,89,75,50,36,43,7,20,45,9,23,10,70,12,81,62,12,51,3,5,96,7,93,90,12,41,5,52,30,91,12,62,34,44,92,68,9,81,9,6,30,38,63,27,51,3,44,47,27,86,41,1,73,78,15,34,98,9,63,66,21,89,96,5,9,36,21,97,6,26,75,14,86,16,82,21,23,91,25,15,66,33,2,50,26,18,61,73,17,49,15,99,19,68,96,33,23,12,81,11,51,19,30,56,74,27,40,76,15,49,11,24,50,27,50,36,77,36,16,22,80,86,11,85,20,87,24,26,6,64,35,27,65,32,86,42,99,30,78,68,24,67,82,4,76,63,36,4,46,21,72,68,17,21,69,71,36,82,22,57,1,29,95,59,18,48,40,91,7,44,22,64,5,52,20,20,86,34,9,67,74,22,13,31,97,23,19,78,19,12,80,19,82,83,11,26,5,10,74,2,42,5,94,26,79,51,33,15,47,9,12,84,20,37,85,63,27,92,16,10,82,64,15,50,75,12,68,51,37,87,10,51,18,11,13,99,97,30,33,48,2,45,29,22,45,20,49,14,78,33,41,89,4,67,21,40,42,20,4,34,64,98,32,77,28,79,9,51,91,58,19,45,56,4,10,3,52,47,65,11,21,53,25,57,78,33,16,70,88,34,56,37,86,30,4,84,91,86,90,37,37,25,59,2,96,25,19,69,6,11,67,83,38,8,49,18,17,21,56,20,43,89,8,78,30,80,52,29,9,65,1,1,65,27,84,23,8,33,99,71,28,38,45,14,40,31,45,44,12,94,12,65,23,96,5,93,50,35,84,10,34,81,2,51,15,11,92,69,20,65,27,68,86,76,36,49,38,79,92,38,72,8,32,80,29,41,7,15,78,38,5,10,61,24,44,38,19,80,9,60,95,95,33,48,13,51,32,57,84,97,1,51,36,6,51,96,16,62,32,13,93,4,79,40,2,68,74,38,4,30,82,17,67,51,68,29,3,85,13,5,2,30,71,36,77,35,78,23,87,22,7,78,5,60,2,11,42,15,68,89,66,93,31,38,31,81,8,65,22,7,27,83,59,21,12,73,64,72,40,38,59,20,29,92,20,7,65,16,86,81,12,44,77,97,30,19,49,61,24,29,24,31,87,89,31,42,80,17,91,23,18,91,10,53,5,17,53,30,96,96,34,83,34,18,68,79,97,18,4,56,37,33,62,31,79,99,32,14,99,87,83,53,34,26,17,70,59,31,12,42,91,32,93,5,54,8,10,83,20,58,92,30,71,24,34,60,3,9,64,72,12,70,14,22,69,38,27,77,31,84,8,54,44,58,9,30,95,22,12,61,95,21,81,71,5,64,44,7,71,4,17,41,2,89,16,20,93,88,20,31,45,28,49,91,15,72,43,6,21,82,15,25,99,8,11,34,18,93,50,15,15,98,27,34,44,38,15,29,79,42,14,86,68,56,7,3,97,21,58,11,33,67,6,53,23,71,16,58,74,17,92,17,14,98,23,35,60,32,70,54,1,82,2,41,32,68,91,27,80,6,25,55,93,23,52,91,3,95,44,3,42,70,23,16,54,36,36,59,5,63,27,40,11,73,34,48,29,73,36,74,77,58,25,55,25,45,7,58,53,49,8,95,13,84,23,58,37,42,6,70,36,58,73,55,14,51,5,99,95,61,20,65,0,0,21,21,1,10,1,0,0,0,0,0,0
@@ -20,7 +31,7 @@ fn main() {
     let (map, oxygen_pos) = run_program(&input_map);
 
     print_map(&map, &[]);
-    println!("Minimum steps {}", find_path((0, 0), &[oxygen_pos], &map).len());
+    println!("Minimum steps {}", find_path((0, 0), oxygen_pos, &map).len());
 
     println!("Part 2: {}", deepest_point(&map, oxygen_pos, oxygen_pos));
 }
@@ -88,7 +99,7 @@ fn adjacent_positions((x, y): (i32, i32)) -> [((i32, i32), Dir); 4] {
 }
 
 fn find_path(
-    start_pos: (i32, i32), end_positions: &[(i32, i32)], map: &HashMap<(i32, i32), Tile>
+    start_pos: (i32, i32), end_pos: (i32, i32), map: &HashMap<(i32, i32), Tile>
 ) -> Vec<((i32, i32), Dir)> {
     let mut queue = VecDeque::new();
     let mut discovered = HashSet::new();
@@ -98,7 +109,7 @@ fn find_path(
     queue.push_back(start_pos);
 
     while let Some(pos) = queue.pop_front() {
-        if end_positions.contains(&pos) {
+        if end_pos == pos {
             let mut path = Vec::new();
 
             let mut pos = pos;
@@ -110,15 +121,11 @@ fn find_path(
             return path;
         }
 
-        if map.get(&pos) == None {
-            // Don't try to find paths through unexplored tiles
-            continue;
-        }
-
         for (new_pos, dir) in &adjacent_positions(pos) {
             if !discovered.contains(new_pos) {
                 discovered.insert(*new_pos);
-                if map.get(new_pos) != Some(&Wall) {
+                let tile = map.get(new_pos);
+                if tile == Some(&Floor) || tile == Some(&Oxygen) {
                     path_to.insert(*new_pos, (pos, *dir));
                     queue.push_back(*new_pos);
                 }
@@ -126,21 +133,8 @@ fn find_path(
         }
     }
 
-    let mut positions = Vec::from(end_positions);
-    positions.push(start_pos);
-    print_map(map, &positions);
-    panic!("No path found from {:?} to {:?}", end_positions, start_pos);
-}
-
-fn add_adjacent_tiles(
-    tiles_to_explore: &mut Vec<(i32, i32)>, pos: (i32, i32), map: &HashMap<(i32, i32), Tile>
-) {
-    let currently_exploring = tiles_to_explore.clone();
-    for (other_position, _) in &adjacent_positions(pos) {
-        if map.get(other_position) == None && !currently_exploring.contains(other_position) {
-            tiles_to_explore.push(*other_position);
-        }
-    }
+    print_map(map, &[start_pos, end_pos]);
+    panic!("No path found from {:?} to {:?}", start_pos, end_pos);
 }
 
 fn run_program(initial_memory: &HashMap<i64, i64>) -> (HashMap<(i32, i32), Tile>, (i32, i32)) {
@@ -150,14 +144,12 @@ fn run_program(initial_memory: &HashMap<i64, i64>) -> (HashMap<(i32, i32), Tile>
 
     let mut robot_position = (0, 0);
     let mut next_position = (0, 0);
+    let mut dir_back = None;
 
     let mut map = HashMap::new();
     map.insert((0, 0), Floor);
     let mut oxygen_position = None;
-
-    let mut tiles_to_explore = Vec::new();
-    add_adjacent_tiles(&mut tiles_to_explore, (0, 0), &map);
-    let mut path_to_follow = Vec::new();
+    let mut backtracking_stack = Vec::new();
 
     macro_rules! get_data {
         ($mode:expr, $parameter:expr) => {
@@ -209,24 +201,28 @@ fn run_program(initial_memory: &HashMap<i64, i64>) -> (HashMap<(i32, i32), Tile>
                 i += 4;
             }
             3 => {
-                let pos = memory[&(i+1)];
-                let (tile, dir) = match path_to_follow.pop() {
-                    Some((tile, dir)) => {
-                        (tile, dir)
+                let mem_loc = memory[&(i+1)];
+                let mut dead_end = true;
+                for (adj, dir) in &adjacent_positions(robot_position) {
+                    if map.get(adj) == None {
+                        dead_end = false;
+                        next_position = *adj;
+                        dir_back = Some(dir.reverse());
+                        set_data!(mode1, mem_loc, *dir as i64);
+                        break;
                     }
-                    None => {
-                        tiles_to_explore.retain(|t| map.get(t) == None);
-                        if tiles_to_explore.len() == 0 {
-                            println!("Nothing left to explore");
-                            return (map, oxygen_position.unwrap());
-                        }
+                }
 
-                        path_to_follow = find_path(robot_position, &tiles_to_explore, &map);
-                        path_to_follow.pop().expect("Could not find a path")
+                if dead_end {
+                    if let Some((pos, dir)) = backtracking_stack.pop() {
+                        next_position = pos;
+                        dir_back = None;
+                        set_data!(mode1, mem_loc, dir as i64);
+                    } else {
+                        println!("Nothing left to explore");
+                        break;
                     }
-                };
-                next_position = tile;
-                set_data!(mode1, pos, dir as i64);
+                }
                 i += 2;
             }
             4 => {
@@ -236,11 +232,16 @@ fn run_program(initial_memory: &HashMap<i64, i64>) -> (HashMap<(i32, i32), Tile>
                     }
                     1 => {
                         map.insert(next_position, Floor);
-                        add_adjacent_tiles(&mut tiles_to_explore, next_position, &map);
+                        if let Some(dir) = dir_back {
+                            backtracking_stack.push((robot_position, dir));
+                        }
                         robot_position = next_position;
                     },
                     2 => {
                         map.insert(next_position, Oxygen);
+                        if let Some(dir) = dir_back {
+                            backtracking_stack.push((robot_position, dir));
+                        }
                         oxygen_position = Some(next_position);
                         robot_position = next_position;
                     }
