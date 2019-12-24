8fa27b76774f18185928bc6ba394e33e430078db
Emil Segerbäck <emilsegerback@gmail.com>
2019-12-24 19:02:38 +0100

    Solve 24b 2019

--- 2019/24.rs
+++ 2019/24.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-type State = Vec<Vec<char>>;
+type State = [[char; 5]; 5];
 
 fn main() {
     let input = "#.###
@@ -10,12 +10,26 @@ fn main() {
 ..#.#";
 
     fn str_to_state(s: &str) -> State {
-        s.split('\n').map(
-            |s| s.chars().collect()
-        ).collect()
+        let state_vec: Vec<[char; 5]> = s.split('\n').map(
+            |s| {
+                let row_vec: Vec<char> = s.chars().collect();
+                let mut row = ['.'; 5];
+                for i in 0..5 {
+                    row[i] = row_vec[i];
+                }
+                row
+            }
+        ).collect();
+
+        let mut state = [['.'; 5]; 5];
+        for i in 0..5 {
+            state[i] = state_vec[i];
+        }
+        state
     }
 
-    let mut state: State = str_to_state(input);
+    let inital_state: State = str_to_state(input);
+    let mut state = inital_state;
 
     let mut old_states = HashSet::new();
     old_states.insert(state.clone());
@@ -30,6 +44,24 @@ fn main() {
         }
         old_states.insert(state.clone());
     }
+
+    let mut recursive_states = vec![[['.'; 5]; 5]; 201];
+    recursive_states[100] = inital_state;
+
+    for _ in 0..200 {
+        tick2(&mut recursive_states);
+    }
+
+    let count = recursive_states.iter().flatten().flatten().fold(0, |sum, c| {
+        if *c == '#' {
+            sum + 1
+        } else {
+            sum
+        }
+    });
+
+    // 1503 too low
+    println!("Part 2: {}", count);
 }
 
 fn print_state(state: &State) {
@@ -91,4 +123,107 @@ fn tick1(state: &State) -> State {
         }
     }
     new_state
+}
+
+fn tick2(recursive_state: &mut Vec<State>) {
+    let old_state = recursive_state.clone();
+    for i in 0..201 {
+        let state = old_state[i];
+
+        for r in 0..5 {
+            for c in 0..5 {
+                if r == 2 && c == 2 {
+                    continue;
+                }
+
+                let mut neighbours = 0;
+
+                fn in_this_state(r: i8, c: i8) -> bool {
+                    if r == 2 && c == 2 {
+                        return false;
+                    }
+
+                    r >= 0 && r < 5 && c >= 0 && c < 5
+                }
+
+                let above_in_state = in_this_state(r as i8 - 1, c as i8);
+                let below_in_state = in_this_state(r as i8 + 1, c as i8);
+                let left_in_state = in_this_state(r as i8, c as i8 - 1);
+                let right_in_state = in_this_state(r as i8, c as i8 + 1);
+
+                if above_in_state && state[r-1][c] == '#' {
+                    neighbours += 1;
+                }
+                if below_in_state && state[r+1][c] == '#' {
+                    neighbours += 1;
+                }
+                if left_in_state && state[r][c-1] == '#' {
+                    neighbours += 1;
+                }
+                if right_in_state && state[r][c+1] == '#' {
+                    neighbours += 1;
+                }
+
+                // Check outer state
+                if i > 0 {
+                    let outer_state = old_state[i-1];
+                    if r == 0 && outer_state[1][2] == '#' {
+                        neighbours += 1;
+                    }
+                    if r == 4 && outer_state[3][2] == '#' {
+                        neighbours += 1;
+                    }
+                    if c == 0 && outer_state[2][1] == '#' {
+                        neighbours += 1;
+                    }
+                    if c == 4 && outer_state[2][3] == '#' {
+                        neighbours += 1;
+                    }
+                }
+
+                // Check inner state
+                if i < 200 {
+                    let inner_state = old_state[i+1];
+                    if r == 1 && c == 2 {
+                        for c in 0..5 {
+                            if inner_state[0][c] == '#' {
+                                neighbours += 1;
+                            }
+                        }
+                    }
+                    if r == 3 && c == 2 {
+                        for c in 0..5 {
+                            if inner_state[4][c] == '#' {
+                                neighbours += 1;
+                            }
+                        }
+                    }
+                    if r == 2 && c == 1 {
+                        for r in 0..5 {
+                            if inner_state[r][0] == '#' {
+                                neighbours += 1;
+                            }
+                        }
+                    }
+                    if r == 2 && c == 3 {
+                        for r in 0..5 {
+                            if inner_state[r][4] == '#' {
+                                neighbours += 1;
+                            }
+                        }
+                    }
+                }
+
+                if state[r][c] == '#' {
+                    if neighbours != 1 {
+                        recursive_state[i][r][c] = '.';
+                    }
+                } else {
+                    if neighbours == 1 || neighbours == 2 {
+                        recursive_state[i][r][c] = '#';
+                    }
+                }
+            }
+        }
+    }
 }
