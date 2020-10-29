4eafb8be88cdb7181f31687c8213f3b083fd38c4
Emil Segerbäck <emilsegerback@mykolab.ch>
2020-10-29 13:06:48 +0100

    Update 1 2015 to work with zig 0.6

--- 2015/1.zig
+++ 2015/1.zig
@@ -1,19 +1,15 @@
 const std = @import("std");
-const fs = std.fs;
 
 fn read_input(allocator: *std.mem.Allocator) ![]u8 {
-    var input_file = try fs.File.openRead("1input");
+    var input_file = try std.fs.cwd().openFile("1input", .{});
     defer input_file.close();
 
-    const in_stream = &input_file.inStream().stream;
+    const in_stream = &input_file.inStream();
     return try in_stream.readAllAlloc(allocator, 8000);
 }
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.direct_allocator);
-    defer arena.deinit();
-
-    const allocator = &arena.allocator;
+    const allocator = std.heap.page_allocator;
 
     var input_string = try read_input(allocator);
 
@@ -38,7 +34,6 @@ pub fn main() !void {
         }
     }
 
-    var stdout_file = try std.io.getStdOut();
-    const stdout = &stdout_file.outStream().stream;
-    try stdout.print("Part 1: {}\nPart 2: {}\n", floor, position);
+    const stdout = std.io.getStdOut().outStream();
+    try stdout.print("Part 1: {}\nPart 2: {}\n", .{ floor, position });
 }
