f2d3646e580f0a446e92b4c10e79620a99ee4288
Emil Segerbäck <emilsegerback@mykolab.ch>
2020-12-07 13:28:21 +0100

    Change 7a/2020 to use iterators

--- 2020/7.rs
+++ 2020/7.rs
@@ -25,25 +25,14 @@ fn main() {
     let mut can_be_contained_by = HashMap::new();
     for (container_bag_type, contains) in &rules {
         for (_, inner_bag_type) in contains {
-            let containers = can_be_contained_by.entry(inner_bag_type).or_insert(Vec::new());
+            let containers = can_be_contained_by.entry(*inner_bag_type).or_insert(Vec::new());
             containers.push(*container_bag_type);
         }
     }
 
     let shiny_gold = ("shiny", "gold");
 
-    // solve part 1
-    let mut can_contain_gold_bag = HashSet::new();
-    let mut contain_stack = vec![shiny_gold];
-    let empty = Vec::new();
-    while let Some(bag_type) = contain_stack.pop() {
-        for container in can_be_contained_by.get(&bag_type).unwrap_or(&empty) {
-            if !can_contain_gold_bag.contains(container) {
-                can_contain_gold_bag.insert(container);
-                contain_stack.push(*container);
-            }
-        }
-    }
+    let can_contain_gold_bag = container_bag_types(shiny_gold, &can_be_contained_by);
     println!("Part 1: {}", can_contain_gold_bag.len());
 
     println!("Part 2: {}", count_bags_inside(shiny_gold, &rules));
@@ -51,9 +40,23 @@ fn main() {
 
 type Bag<'a> = (&'a str, &'a str);
 
+fn container_bag_types<'a>(
+    bag: Bag<'a>,
+    can_be_contained_by: &'a HashMap<Bag<'a>, Vec<Bag<'a>>>,
+) -> HashSet<&'a Bag<'a>> {
+    if let Some(containers) = can_be_contained_by.get(&bag) {
+        return containers
+            .iter()
+            .flat_map(|c| container_bag_types(*c, can_be_contained_by))
+            .chain(containers.iter())
+            .collect();
+    }
+    return HashSet::new();
+}
+
 fn count_bags_inside<'a>(
     bag: Bag<'a>,
-    rules: &HashMap<Bag<'a>, Vec<(usize, Bag<'a>)>>
+    rules: &HashMap<Bag<'a>, Vec<(usize, Bag<'a>)>>,
 ) -> usize {
     let mut total = 0;
     if let Some(bags_inside) = rules.get(&bag) {
