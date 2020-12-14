f6e003d9f1abbd6c7ffc6f3d581c4637a27a5943
Emil Segerbäck <emilsegerback@gmail.com>
2020-12-14 17:44:43 +0100

    Solve 13b 2020

--- 2020/13.rs
+++ 2020/13.rs
@@ -3,14 +3,8 @@ use std::fs;
 fn main() {
     let contents = fs::read_to_string("13input").unwrap();
     let earliest_possible: usize = contents.lines().nth(0).unwrap().parse().unwrap();
-
-    let buses: Vec<usize> = contents
-        .lines()
-        .nth(1)
-        .unwrap()
-        .split(',')
-        .filter_map(|s| s.parse().ok())
-        .collect();
+    let second_line = contents.lines().nth(1).unwrap();
+    let buses: Vec<usize> = second_line.split(',').filter_map(|s| s.parse().ok()).collect();
 
     let earliest_after = buses.iter().map(|bus| {
         let time = (earliest_possible / bus) * bus;
@@ -23,4 +17,39 @@ fn main() {
 
     let (i, t) = earliest_after.enumerate().min_by_key(|(_, t)| *t).unwrap();
     println!("Part 1: {}", (t - earliest_possible) * buses[i]);
+
+    let mut rules = second_line.split(',').enumerate().filter_map(|(offset, bus)| {
+        if let Ok(b) = bus.parse() {
+            Some((offset, b))
+        } else {
+            None
+        }
+    });
+
+    let (mut time, mut bus) = rules.next().unwrap();
+    for (offset, b) in rules {
+        loop {
+            time += bus;
+            if (time + offset) % b == 0 {
+                break;
+            }
+        }
+
+        bus = lcm(bus, b);
+    }
+
+    println!("Part 2: {}", time);
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    a / gcd(a, b) * b
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    while b != 0 {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    a
 }
