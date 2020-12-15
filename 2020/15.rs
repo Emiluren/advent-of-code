83df6c210dc496e37d083baf1497d4e1fcdb2128
Emil Segerbäck <emilsegerback@gmail.com>
2020-12-15 21:20:36 +0100

    Use a Vec instead of HashMap in 15/2020

--- 2020/15.rs
+++ 2020/15.rs
@@ -1,19 +1,19 @@
-use std::collections::HashMap;
-
 fn main() {
     let input = [0,8,15,2,12,1,4];
 
-    let mut time_latest: HashMap<usize, usize> =
-        input.iter().copied().enumerate().map(|(i, n)| (n, i)).collect();
+    let mut time_latest = vec![None; 30_000_000];
+    for (i, n) in input.iter().enumerate() {
+        time_latest[*n] = Some(i);
+    }
 
     let mut last_num = input[input.len() - 1];
-    for i in input.len()..30_000_000 {
+    for i in input.len()..time_latest.len() {
         if i == 2020 {
             println!("Part 1: {}", last_num);
         }
 
-        let num = time_latest.get(&last_num).map(|j| i - j - 1).unwrap_or(0);
-        time_latest.insert(last_num, i - 1);
+        let num = time_latest[last_num].map(|j| i - j - 1).unwrap_or(0);
+        time_latest[last_num] = Some(i - 1);
         last_num = num;
     }
 
