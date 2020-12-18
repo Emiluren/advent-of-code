43948f7bf7568c2b4d8a82d87ee2b68822ebdd09
Emil Segerbäck <emilsegerback@gmail.com>
2020-12-18 14:41:54 +0100

    Solve 18b 2020

--- 2020/18.rs
+++ 2020/18.rs
@@ -1,77 +1,76 @@
-enum Expr {
-    Num(u64),
-    Add,
-    Mul,
-    Paren(Vec<Expr>)
-}
-use Expr::*;
+use std::collections::VecDeque;
 
 fn main() {
     let input = std::fs::read_to_string("18input").unwrap();
 
     let mut sum1 = 0;
+    let mut sum2 = 0;
     for line in input.lines() {
-        let mut chars = line.chars();
-        let expr = parse(&mut chars);
-        sum1 += eval_1(expr);
+        sum1 += eval_rpn(to_rpn(line.chars(), false));
+        sum2 += eval_rpn(to_rpn(line.chars(), true));
     }
     println!("Part 1: {}", sum1);
+    println!("Part 2: {}", sum2);
 }
 
-fn parse(chars: &mut impl Iterator<Item = char>) -> Vec<Expr> {
-    let mut expressions = Vec::new();
-    while let Some(c) = chars.next() {
-        match c {
-            ')' => {
-                break;
+// Using shunting-yard algorithm (https://en.wikipedia.org/wiki/Shunting-yard_algorithm)
+fn to_rpn(chars: impl Iterator<Item = char>, part2: bool) -> impl Iterator<Item = char> {
+    let mut output_queue = VecDeque::new();
+    let mut operator_stack = Vec::new();
+    for c in chars {
+        if c.is_digit(10) {
+            output_queue.push_back(c);
+        } else if c == '+' || c == '*' {
+            while let Some(op) = operator_stack.last().copied() {
+                if op == '+' || op == '*' && (!part2 || c == '*') {
+                    output_queue.push_back(op);
+                    operator_stack.pop().unwrap();
+                } else {
+                    break;
+                }
             }
-            '+' => {
-                expressions.push(Add)
+            operator_stack.push(c)
+        } else if c == '(' {
+            operator_stack.push(c);
+        } else if c == ')' {
+            while let Some(op) = operator_stack.last().copied() {
+                if op != '(' {
+                    output_queue.push_back(op);
+                    operator_stack.pop().unwrap();
+                } else {
+                    break;
+                }
             }
-            '*' => {
-                expressions.push(Mul)
-            }
-            '(' => {
-                expressions.push(Paren(parse(chars)));
-            }
-            _ => {
-                expressions.push(Num(c.to_digit(10).unwrap() as u64));
-            }
-        }
-    }
-    expressions
-}
-
-fn eval_1(exprs: impl IntoIterator<Item = Expr>) -> u64 {
-    let mut value = None;
-    let mut op = None;
-    for e in exprs {
-        let mut new_v = None;
-        match e {
-            Add => {
-                assert!(value != None);
-                op = Some((|a,b| a+b) as fn(u64, u64) -> u64);
-            }
-            Mul => {
-                assert!(value != None);
-                op = Some((|a,b| a*b) as fn(u64, u64) -> u64);
-            }
-            Num(n) => {
-                new_v = Some(n);
-            }
-            Paren(es) => {
-                new_v = Some(eval_1(es));
-            }
-        }
-
-        if let Some(v) = new_v {
-            if let Some(old_v) = value {
-                value = Some(op.unwrap()(v, old_v));
+            if operator_stack.last().copied() != Some('(') {
+                panic!("Unbalanced parentheses");
             } else {
-                value = Some(v);
+                operator_stack.pop().unwrap();
             }
-            op = None;
+        } else {
+            panic!("Unknown token: {}", c);
         }
     }
-    value.unwrap()
+    while let Some(op) = operator_stack.pop() {
+        output_queue.push_back(op);
+    }
+    output_queue.into_iter()
+}
+
+fn eval_rpn(chars: impl Iterator<Item = char>) -> u64 {
+    let mut value_stack = Vec::new();
+    for c in chars {
+        if let Some(d) = c.to_digit(10) {
+            value_stack.push(d as u64)
+        } else if c == '+' {
+            let v1 = value_stack.pop().unwrap();
+            let v2 = value_stack.pop().unwrap();
+            value_stack.push(v1 + v2);
+        } else {
+            assert_eq!(c, '*');
+            let v1 = value_stack.pop().unwrap();
+            let v2 = value_stack.pop().unwrap();
+            value_stack.push(v1 * v2);
+        }
+    }
+    value_stack.pop().unwrap()
 }
