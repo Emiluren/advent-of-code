92ff6f7dc41cbeedb4d7598187091f6ceb19ca76
Emil Segerbäck <emil.segerback@polisen.se>
2021-07-30 15:28:16 +0200

    Solve 5b 2015

--- 2015/5.zig
+++ 2015/5.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 
 pub fn main() !void {
+    try part1();
+    try part2();
+}
+
+fn part1() !void {
     var input_file = try std.fs.cwd().openFile("5input", .{});
     defer input_file.close();
 
@@ -50,6 +55,54 @@ pub fn main() !void {
             nice_strings += 1;
         }
     }
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Part 1: {}\n", .{ nice_strings });
+}
+
+fn part2() !void {
+    var input_file = try std.fs.cwd().openFile("5input", .{});
+    defer input_file.close();
+
+    const reader = &input_file.reader();
+
+    var nice_strings: usize = 0;
+    while (true) {
+        var buffer: [16]u8 = undefined;
+        const read_bytes = try reader.read(&buffer);
+        if (read_bytes < 16) {
+            break;
+        }
+        try reader.skipBytes(1, .{});
+
+        var found_pair = false;
+        var i: usize = 0;
+        pair_loop: while (i < 13) : (i += 1) {
+            var j = i + 2;
+            while (j < 15) : (j += 1) {
+                if (buffer[i] == buffer[j] and buffer[i+1] == buffer[j+1]) {
+                    found_pair = true;
+                    break :pair_loop;
+                }
+            }
+        }
+        if (!found_pair) {
+            continue;
+        }
+
+        var found_repeat = false;
+        i = 0;
+        while (i < 14) : (i += 1) {
+            if (buffer[i] == buffer[i + 2]) {
+                found_repeat = true;
+                break;
+            }
+        }
+
+        if (found_repeat) {
+            nice_strings += 1;
+        }
+    }
+
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Part 1: {}\n", .{ nice_strings });
 }
