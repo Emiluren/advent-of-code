00495f35f7b135b55653b924bfc0c2b98504fc57
Emil Segerbäck <emilsegerback@gmail.com>
2021-12-21 22:19:03 +0100

    Continue translating 18/2021 to rust

--- 2021/18.rs
+++ 2021/18.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::rc::Rc;
 
-enum Snailfish { Pair(Box<Snailfish>, Box<Snailfish>), V(u8) }
+enum Snailfish { Pair(Rc<Snailfish>, Rc<Snailfish>), V(u8) }
 use Snailfish::*;
 
 impl std::fmt::Display for Snailfish {
@@ -13,37 +14,6 @@ impl std::fmt::Display for Snailfish {
     }
 }
 
-impl Snailfish {
-    fn explode(&self, depth: u8) -> Option<(Snailfish, Option<u8>, Option<u8>)> {
-        match self {
-            V(_) => None,
-            Pair(sn1, sn2) => {
-                if depth > 4 {
-                    if let (V(v1), V(v2)) = (*sn1, *sn2) {
-                        return Some((V(0), Some(v1), Some(v2)));
-                    }
-                }
-
-                if let Some((new_left, add_left, add_right)) = sn1.explode(depth+1) {
-                    Some((
-                        Pair(Box::new(new_left), Box::new(add_deep_left(*sn2, add_right))),
-                        add_left,
-                        None,
-                    ))
-                } else if let Some((new_right, add_left, add_right)) = sn2.explode(depth+1) {
-                    Some((
-                        Pair(Box::new(add_deep_right(*sn1, add_left)), Box::new(new_right)),
-                        None,
-                        add_right,
-                    ))
-                } else {
-                    None
-                }
-            }
-        }
-    }
-}
-
 fn parse_snailfish(chars: &mut std::iter::Peekable<std::str::Chars<'_>>) -> Snailfish {
     if *chars.peek().unwrap() == '[' {
         chars.next().unwrap();
@@ -51,7 +21,7 @@ fn parse_snailfish(chars: &mut std::iter::Peekable<std::str::Chars<'_>>) -> Snai
         assert!(chars.next().unwrap() == ',');
         let sn2 = parse_snailfish(chars);
         assert!(chars.next().unwrap() == ']');
-        Pair(Box::new(sn1), Box::new(sn2))
+        Pair(Rc::new(sn1), Rc::new(sn2))
     } else {
         V(chars.next().unwrap().to_string().parse().unwrap())
     }
@@ -64,21 +34,56 @@ fn read_from_input_file() -> std::io::Result<Vec<Snailfish>> {
     Ok(contents.lines().map(|line| parse_snailfish(&mut line.chars().peekable())).collect())
 }
 
-fn add_deep_left(sf: Snailfish, add: Option<u8>) -> Snailfish {
-    match (sf, add) {
-        (sf, None) => sf,
-        (Pair(sn1, sn2), Some(add)) =>
-            Pair(Box::new(add_deep_left(*sn1, Some(add))), sn2),
-        (V(v), Some(add)) => V(v + add),
+fn add_deep_left(sf: &Snailfish, add: u8) -> Snailfish {
+    match sf {
+        Pair(sn1, sn2) =>
+            Pair(Rc::new(add_deep_left(sn1, add)), sn2.clone()),
+        V(v) => V(v + add),
     }
 }
 
-fn add_deep_right(sf: Snailfish, add: Option<u8>) -> Snailfish {
-    match (sf, add) {
-        (sf, None) => sf,
-        (Pair(sn1, sn2), Some(add)) =>
-            Pair(sn1, Box::new(add_deep_right(*sn2, Some(add)))),
-        (V(v), Some(add)) => V(v + add),
+fn add_deep_right(sf: &Snailfish, add: u8) -> Snailfish {
+    match sf {
+        Pair(sn1, sn2) =>
+            Pair(sn1.clone(), Rc::new(add_deep_right(sn2, add))),
+        V(v) => V(v + add),
+    }
+}
+
+impl Snailfish {
+    fn explode(&self, depth: u8) -> Option<(Snailfish, u8, u8)> {
+        match self {
+            V(_) => None,
+            Pair(sn1, sn2) => {
+                if depth > 4 {
+                    if let (V(ref v1), V(ref v2)) = (sn1.as_ref(), sn2.as_ref()) {
+                        return Some((V(0), *v1, *v2));
+                    }
+                }
+
+                if let Some((new_left, add_left, add_right)) = sn1.explode(depth+1) {
+                    Some((
+                        Pair(
+                            Rc::new(new_left),
+                            Rc::new(add_deep_left(sn2, add_right)),
+                        ),
+                        add_left,
+                        0,
+                    ))
+                } else if let Some((new_right, add_left, add_right)) = sn2.explode(depth+1) {
+                    Some((
+                        Pair(
+                            Rc::new(add_deep_right(sn1, add_left)),
+                            Rc::new(new_right),
+                        ),
+                        0,
+                        add_right,
+                    ))
+                } else {
+                    None
+                }
+            }
+        }
     }
 }
 
