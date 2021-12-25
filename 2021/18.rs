060de2f5e8a8325e1466dd82b6cad4164c84534b
Emil Segerbäck <emilsegerback@gmail.com>
2021-12-25 23:19:26 +0100

    Optimize rust solution of 18/2021

--- 2021/18.rs
+++ 2021/18.rs
@@ -1,100 +1,86 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::rc::Rc;
 
-#[derive(Clone)]
-enum Snailfish { Pair(Rc<Snailfish>, Rc<Snailfish>), V(u8) }
+#[derive(Copy, Clone)]
+enum Snailfish { Pair(usize, usize), V(u8) }
 use Snailfish::*;
 
-impl std::fmt::Display for Snailfish {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Pair(sn1, sn2) => write!(f, "[{},{}]", *sn1, sn2),
-            V(v) => write!(f, "{}", v),
-        }
-    }
-}
-
-fn parse_snailfish(chars: &mut std::str::Chars<'_>) -> Result<Snailfish, String> {
+fn parse_snailfish(chars: &mut std::str::Chars<'_>, fishes: &mut Vec<Snailfish>) -> Result<usize, String> {
     match chars.next() {
         Some('[') => {
-            let sn1 = parse_snailfish(chars)?;
+            let sn1 = parse_snailfish(chars, fishes)?;
             if chars.next() != Some(',') {
                 return Err("Expected ,".to_string());
             }
-            let sn2 = parse_snailfish(chars)?;
+            let sn2 = parse_snailfish(chars, fishes)?;
             if chars.next() != Some(']') {
                 return Err("Expected ]".to_string());
             }
-            Ok(Pair(Rc::new(sn1), Rc::new(sn2)))
+            fishes.push(Pair(sn1, sn2));
         }
-        Some(c) => Ok(V(c.to_string().parse::<u8>().map_err(|e| e.to_string())?)),
+        Some(c) => {
+            fishes.push(V(c.to_string().parse::<u8>().map_err(|e| e.to_string())?));
+        }
         None => {
-            Err("Expected [ or number".to_string())
+            return Err("Expected [ or number".to_string());
         }
     }
+    Ok(fishes.len() - 1)
 }
 
-impl std::str::FromStr for Snailfish {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        parse_snailfish(&mut s.chars())
-    }
-}
-
-fn read_from_input_file() -> std::io::Result<Vec<Snailfish>> {
+fn read_from_input_file() -> std::io::Result<(Vec<usize>, Vec<Snailfish>)> {
     let mut file = File::open("18input")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
-    Ok(contents.lines().map(|line| line.parse().unwrap()).collect())
+
+    let mut fishes = Vec::new();
+    let mut top = Vec::new();
+    for line in contents.lines() {
+        top.push(parse_snailfish(&mut line.chars(), &mut fishes).unwrap());
+    }
+    Ok((top, fishes))
 }
 
-fn add_deep_left(sf: &Snailfish, add: u8) -> Snailfish {
-    match sf {
+fn add_deep_left(sf: Snailfish, add: u8, fishes: &mut Vec<Snailfish>) -> usize {
+    let new_fish = match sf {
         Pair(sn1, sn2) =>
-            Pair(Rc::new(add_deep_left(sn1, add)), sn2.clone()),
+            Pair(add_deep_left(fishes[sn1], add, fishes), sn2),
         V(v) => V(v + add),
-    }
+    };
+    fishes.push(new_fish);
+    fishes.len() - 1
 }
 
-fn add_deep_right(sf: &Snailfish, add: u8) -> Snailfish {
-    match sf {
+fn add_deep_right(sf: Snailfish, add: u8, fishes: &mut Vec<Snailfish>) -> usize {
+    let new_fish = match sf {
         Pair(sn1, sn2) =>
-            Pair(sn1.clone(), Rc::new(add_deep_right(sn2, add))),
+            Pair(sn1, add_deep_right(fishes[sn2], add, fishes)),
         V(v) => V(v + add),
-    }
+    };
+    fishes.push(new_fish);
+    fishes.len() - 1
 }
 
 impl Snailfish {
-    fn explode(&self, depth: u8) -> Option<(Snailfish, u8, u8)> {
+    fn explode(self, depth: u8, fishes: &mut Vec<Snailfish>) -> Option<(usize, u8, u8)> {
         match self {
             V(_) => None,
             Pair(sn1, sn2) => {
                 if depth > 4 {
-                    if let (V(ref v1), V(ref v2)) = (sn1.as_ref(), sn2.as_ref()) {
-                        return Some((V(0), *v1, *v2));
+                    if let (V(v1), V(v2)) = (fishes[sn1], fishes[sn2]) {
+                        fishes.push(V(0));
+                        return Some((fishes.len() - 1, v1, v2));
                     }
                 }
 
-                if let Some((new_left, add_left, add_right)) = sn1.explode(depth+1) {
-                    Some((
-                        Pair(
-                            Rc::new(new_left),
-                            Rc::new(add_deep_left(sn2, add_right)),
-                        ),
-                        add_left,
-                        0,
-                    ))
-                } else if let Some((new_right, add_left, add_right)) = sn2.explode(depth+1) {
-                    Some((
-                        Pair(
-                            Rc::new(add_deep_right(sn1, add_left)),
-                            Rc::new(new_right),
-                        ),
-                        0,
-                        add_right,
-                    ))
+                if let Some((new_left, add_left, add_right)) = fishes[sn1].explode(depth+1, fishes) {
+                    let new_fish = Pair(new_left, add_deep_left(fishes[sn2], add_right, fishes));
+                    fishes.push(new_fish);
+                    Some((fishes.len() - 1, add_left, 0))
+                } else if let Some((new_right, add_left, add_right)) = fishes[sn2].explode(depth+1, fishes) {
+                    let new_fish = Pair(add_deep_right(fishes[sn1], add_left, fishes), new_right);
+                    fishes.push(new_fish);
+                    Some((fishes.len() - 1, 0, add_right))
                 } else {
                     None
                 }
@@ -102,20 +88,27 @@ impl Snailfish {
         }
     }
 
-    fn split(&self) -> Option<Snailfish> {
+    fn split(self, fishes: &mut Vec<Snailfish>) -> Option<usize> {
         match self {
             V(v) => {
-                if *v >= 10 {
-                    Some(Pair(Rc::new(V(v/2)), Rc::new(V(v/2 + v%2))))
+                if v >= 10 {
+                    let sn1 = fishes.len();
+                    fishes.push(V(v/2));
+                    let sn2 = fishes.len();
+                    fishes.push(V(v/2 + v%2));
+                    fishes.push(Pair(sn1, sn2));
+                    Some(fishes.len() - 1)
                 } else {
                     None
                 }
             }
             Pair(sn1, sn2) => {
-                if let Some(new_sn1) = sn1.split() {
-                    Some(Pair(Rc::new(new_sn1), sn2.clone()))
-                } else if let Some(new_sn2) = sn2.split() {
-                    Some(Pair(sn1.clone(), Rc::new(new_sn2)))
+                if let Some(new_sn1) = fishes[sn1].split(fishes) {
+                    fishes.push(Pair(new_sn1, sn2));
+                    Some(fishes.len() - 1)
+                } else if let Some(new_sn2) = fishes[sn2].split(fishes) {
+                    fishes.push(Pair(sn1, new_sn2));
+                    Some(fishes.len() - 1)
                 } else {
                     None
                 }
@@ -123,53 +116,55 @@ impl Snailfish {
         }
     }
 
-    fn reduce(&self) -> Snailfish {
-        let mut fish = self.clone();
-        while let Some(newfish) = fish.explode(1).map(|(f, _, _)| f).or_else(|| fish.split()) {
-            fish = newfish;
+    fn reduce(mut i: usize, fishes: &mut Vec<Snailfish>) -> usize {
+        let mut fish = fishes[i];
+        while let Some(new_i) = fish.explode(1, fishes).map(|(f, _, _)| f).or_else(|| fish.split(fishes)) {
+            i = new_i;
+            fish = fishes[i];
         }
-        fish
+        i
     }
 
-    fn magnitude(&self) -> u16 {
+    fn magnitude(self, fishes: &mut Vec<Snailfish>) -> u16 {
         match self {
-            V(v) => *v as u16,
-            Pair(sn1, sn2) => sn1.magnitude()*3 + sn2.magnitude()*2,
+            V(v) => v as u16,
+            Pair(sn1, sn2) => fishes[sn1].magnitude(fishes)*3 + fishes[sn2].magnitude(fishes)*2,
         }
     }
 }
 
-fn part1(input: &[Snailfish]) -> u16 {
-    let mut last_fish: Option<Snailfish> = None;
-    for snailfish in input {
+fn part1(top: &[usize], fishes: &mut Vec<Snailfish>) -> u16 {
+    let mut last_fish: Option<usize> = None;
+    for i in top {
         let sf = match last_fish {
-            Some(last_fish) => Pair(Rc::new(last_fish), Rc::new(snailfish.clone())),
-            None => snailfish.clone(),
+            Some(last_fish) => {
+                fishes.push(Pair(last_fish, *i));
+                fishes.len() - 1
+            },
+            None => *i,
         };
-        last_fish = Some(sf.reduce());
+        last_fish = Some(Snailfish::reduce(sf, fishes));
     }
-    last_fish.unwrap().magnitude()
+    fishes[last_fish.unwrap()].magnitude(fishes)
 }
 
-fn part2(input: &[Snailfish]) -> u16 {
-    input.iter().map(|f1| {
-        input.iter().map(|f2| {
-            Pair(Rc::new(f1.clone()), Rc::new(f2.clone())).reduce().magnitude()
+fn part2(top: &[usize], fishes: &mut Vec<Snailfish>) -> u16 {
+    top.iter().map(|f1| {
+        top.iter().map(|f2| {
+            fishes.push(Pair(*f1, *f2));
+            let reduced = Snailfish::reduce(fishes.len() - 1, fishes);
+            fishes[reduced].magnitude(fishes)
         }).max().unwrap()
     }).max().unwrap()
 }
 
 fn main() -> std::io::Result<()> {
-    let input = read_from_input_file()?;
+    let (top, mut fishes) = read_from_input_file()?;
+    let top2 = top.clone();
+    let mut fishes2 = fishes.clone();
 
-    // let sn = "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]".parse::<Snailfish>().unwrap();
-    // println!("{}", sn);
-    // let sn2 = sn.explode(1).unwrap().0;
-    // println!("{}", sn2);
-    // let sn3 = sn2.explode(1).unwrap().0;
-    // println!("{}", sn3);
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
+    println!("Part 1: {}", part1(&top, &mut fishes));
+    println!("Part 2: {}", part2(&top2, &mut fishes2));
 
     Ok(())
 }
