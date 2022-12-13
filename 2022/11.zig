a4dbc9af064c4c8e5fe659346e3466a46ee8ece6
Emil Segerbäck <emilsegerback@mykolab.ch>
2022-12-13 22:15:12 +0100

    Continue 11/2022

--- 2022/11.zig
+++ 2022/11.zig
@@ -1,10 +1,14 @@
 const std = @import("std");
 
-const Item = u32;
+const Item = u128;
 
 const Operation = struct {
     op_fun: *const fn(old: Item, constant: Item) Item,
     constant: Item,
+
+    fn apply(self: *Operation, it: Item) Item {
+        return self.op_fun(it, self.constant);
+    }
 };
 
 fn sqFun(old: Item, constant: Item) Item {
@@ -40,11 +44,27 @@ pub fn mul(comptime constant: Item) Operation {
 }
 
 const Monkey = struct {
-    items: std.ArrayList(Item),
+    items: Queue(Item),
     operation: Operation,
     test_den: Item,
     true_monkey: usize,
     false_monkey: usize,
+    inspections: usize,
+
+    fn init(item_slice: []const Item, op: Operation, den: Item, true_monkey: usize, false_monkey: usize, allocator: std.mem.Allocator) !Monkey {
+        var items = Queue(Item).init(allocator);
+        for (item_slice) |it| {
+            try items.enqueue(it);
+        }
+        return .{
+            .items = items,
+            .operation = op,
+            .test_den = den,
+            .true_monkey = true_monkey,
+            .false_monkey = false_monkey,
+            .inspections = 0,
+        };
+    }
 };
 
 pub fn main() !void {
@@ -53,9 +73,88 @@ pub fn main() !void {
     const allocator = arena.allocator();
 
     var example_monkeys = [_]Monkey{
-        .{
-            .items = try std.ArrayList(Item).init(allocator).appendSlice([_]Item{79, 98}),
-        },
+        try Monkey.init(&[_]Item{79, 98}, mul(19), 23, 2, 3, allocator),
+        try Monkey.init(&[_]Item{54, 65, 75, 74}, add(6), 19, 2, 0, allocator),
+        try Monkey.init(&[_]Item{79, 60, 97}, square, 13, 1, 3, allocator),
+        try Monkey.init(&[_]Item{74, 98}, add(3), 17, 0, 1, allocator),
     };
-    _ = example_monkeys;
+    var monkeys = example_monkeys;
+
+    var round: usize = 0;
+    while (round < 20) : (round += 1) {
+        for (monkeys) |*m, i| {
+            while (m.items.dequeue()) |it| {
+                m.inspections += 1;
+                const worry = m.operation.apply(it) / 3;
+                const receiver = if (worry % m.test_den == 0) m.true_monkey else m.false_monkey;
+                try monkeys[receiver].items.enqueue(worry);
+                std.debug.print("{} threw {} to {}\n", .{i, worry, receiver});
+            }
+        }
+
+        std.debug.print("Round {}\n", .{round+1});
+        for (monkeys) |*m, i| {
+            std.debug.print("Monkey {}: ", .{i});
+            m.items.print();
+        }
+        std.debug.print("\n", .{});
+    }
+
+    var max = [_]usize{0, 0};
+    for (monkeys) |m| {
+        std.debug.print("{}\n", .{m.inspections});
+        if (m.inspections > max[1]) {
+            max[0] = max[1];
+            max[1] = m.inspections;
+        } else if (m.inspections > max[0]) {
+            max[0] = m.inspections;
+        }
+    }
+    std.debug.print("Part 1: {}\n", .{max[0] * max[1]});
+}
+
+pub fn Queue(comptime Child: type) type {
+    return struct {
+        const This = @This();
+        const Node = struct {
+            data: Child,
+            next: ?*Node,
+        };
+        gpa: std.mem.Allocator,
+        start: ?*Node,
+        end: ?*Node,
+
+        pub fn init(gpa: std.mem.Allocator) This {
+            return This{
+                .gpa = gpa,
+                .start = null,
+                .end = null,
+            };
+        }
+        pub fn enqueue(this: *This, value: Child) !void {
+            const node = try this.gpa.create(Node);
+            node.* = .{ .data = value, .next = null };
+            if (this.end) |end| end.next = node //
+            else this.start = node;
+            this.end = node;
+        }
+        pub fn dequeue(this: *This) ?Child {
+            const start = this.start orelse return null;
+            defer this.gpa.destroy(start);
+            if (start.next) |next|
+                this.start = next
+            else {
+                this.start = null;
+                this.end = null;
+            }
+            return start.data;
+        }
+        pub fn print(this: *This) void {
+            var node = this.start;
+            while (node) |n| : (node = n.next) {
+                std.debug.print("{}, ", .{n.data});
+            }
+            std.debug.print("\n", .{});
+        }
+    };
 }
