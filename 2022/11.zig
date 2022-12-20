81ef265561f179864d17a0bd1c4afab2f378e614
Emil Segerbäck <emilsegerback@gmail.com>
2022-12-20 14:11:46 +0100

    Solve 11a/2022

--- 2022/11.zig
+++ 2022/11.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const Item = u128;
+const Item = u32;
 
 const Operation = struct {
     op_fun: *const fn(old: Item, constant: Item) Item,
@@ -72,37 +72,53 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    var example_monkeys = [_]Monkey{
-        try Monkey.init(&[_]Item{79, 98}, mul(19), 23, 2, 3, allocator),
-        try Monkey.init(&[_]Item{54, 65, 75, 74}, add(6), 19, 2, 0, allocator),
-        try Monkey.init(&[_]Item{79, 60, 97}, square, 13, 1, 3, allocator),
-        try Monkey.init(&[_]Item{74, 98}, add(3), 17, 0, 1, allocator),
+    // var monkeys = [_]Monkey{
+    //     try Monkey.init(&[_]Item{79, 98}, mul(19), 23, 2, 3, allocator),
+    //     try Monkey.init(&[_]Item{54, 65, 75, 74}, add(6), 19, 2, 0, allocator),
+    //     try Monkey.init(&[_]Item{79, 60, 97}, square, 13, 1, 3, allocator),
+    //     try Monkey.init(&[_]Item{74}, add(3), 17, 0, 1, allocator),
+    // };
+    var monkeys = [_]Monkey{
+        try Monkey.init(&[_]Item{54, 82, 90, 88, 86, 54}, mul(7), 11, 2, 6, allocator),
+        try Monkey.init(&[_]Item{91, 65}, mul(13), 5, 7, 4, allocator),
+        try Monkey.init(&[_]Item{62, 54, 57, 92, 83, 63, 63}, add(1), 7, 1, 7, allocator),
+        try Monkey.init(&[_]Item{67, 72, 68}, square, 2, 0, 6, allocator),
+        try Monkey.init(&[_]Item{68, 89, 90, 86, 84, 57, 72, 84}, add(7), 17, 3, 5, allocator),
+        try Monkey.init(&[_]Item{79, 83, 64, 58}, add(6), 13, 3, 0, allocator),
+        try Monkey.init(&[_]Item{96, 72, 89, 70, 88}, add(4), 3, 1, 2, allocator),
+        try Monkey.init(&[_]Item{79}, add(8), 19, 4, 5, allocator),
     };
-    var monkeys = example_monkeys;
+
+    var lcd: Item = 1;
+    for (monkeys) |m| {
+        lcd *= m.test_den;
+    }
+
+    std.debug.print("lcd = {}\n", .{lcd});
 
     var round: usize = 0;
     while (round < 20) : (round += 1) {
-        for (monkeys) |*m, i| {
+        for (monkeys) |*m| {
             while (m.items.dequeue()) |it| {
                 m.inspections += 1;
-                const worry = m.operation.apply(it) / 3;
+                const worry = (m.operation.apply(it) / 3) % lcd;
                 const receiver = if (worry % m.test_den == 0) m.true_monkey else m.false_monkey;
                 try monkeys[receiver].items.enqueue(worry);
-                std.debug.print("{} threw {} to {}\n", .{i, worry, receiver});
+                //std.debug.print("{} threw {} to {}\n", .{i, worry, receiver});
             }
         }
 
-        std.debug.print("Round {}\n", .{round+1});
-        for (monkeys) |*m, i| {
-            std.debug.print("Monkey {}: ", .{i});
-            m.items.print();
-        }
-        std.debug.print("\n", .{});
+        // std.debug.print("Round {}\n", .{round+1});
+        // for (monkeys) |*m, i| {
+        //     std.debug.print("Monkey {}: ", .{i});
+        //     m.items.print();
+        // }
+        // std.debug.print("\n", .{});
     }
 
     var max = [_]usize{0, 0};
     for (monkeys) |m| {
-        std.debug.print("{}\n", .{m.inspections});
+        //std.debug.print("{}\n", .{m.inspections});
         if (m.inspections > max[1]) {
             max[0] = max[1];
             max[1] = m.inspections;
