4fb3c24fb9909114423347013dd9d1839e37c16b
Emil Segerbäck <emilsegerback@mykolab.ch>
2023-12-08 12:33:17 +0100

    Naive solution to 8b/2023 (too slow)

--- 2023/8.lisp
+++ 2023/8.lisp
@@ -12,14 +12,41 @@
         for right-node = (subseq line 12 15)
         collect `(,(intern node) . (,(intern left-node) . ,(intern right-node)))))
 
-(defun part1 ()
+(defun steps-to-reach (start ends)
   (loop
-    with node = 'AAA
+    with node = start
     for i from 0
     for c = (elt *path* (mod i (length *path*)))
     for (left . right) = (alexandria:assoc-value *network* node)
     do (setf node (if (char= c #\L)
                       left
                       right))
-    while (not (eq node 'ZZZ))
+    while (not (member node ends))
+    finally (return (1+ i))))
+
+(defun part1 ()
+  (steps-to-reach 'AAA '(ZZZ)))
+
+(defun collect-ends-with (c)
+  (loop
+    for (n) in *network*
+    if (char= (elt (string n) 2) c)
+      collect n))
+
+(defparameter *start-nodes* (collect-ends-with #\A))
+(defparameter *end-nodes* (collect-ends-with #\Z))
+
+(defun part2 ()
+  (loop
+    with nodes = *start-nodes*
+    for i from 0
+    for c = (elt *path* (mod i (length *path*)))
+    do (setf nodes
+             (loop
+               for n in nodes
+               for (left . right) = (alexandria:assoc-value *network* n)
+               collect (if (char= c #\L)
+                           left
+                           right)))
+    while (some (lambda (n) (not (member n *end-nodes*))) nodes)
     finally (return (1+ i))))
