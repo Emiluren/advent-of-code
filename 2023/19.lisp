7c98c9ed05174505cf1b2dc9de9a7583597cd6ed
Emil Segerbäck <emil@segerback.ninja>
2023-12-20 00:47:46 +0100

    Solve 19a/2023

--- 2023/19.lisp
+++ 2023/19.lisp
@@ -8,17 +8,32 @@
   `(let ((,var ,test-form))
      (if ,var ,then-form ,else-form)))
 
+(defun char-parser (retval)
+  (lambda (stream char)
+    (declare (ignore stream char))
+    retval))
+
 (defun parse-rule (rule-str)
-  (ppcre:all-matches-as-strings "([xmas])|([<>])|([0-9]+)|(:)|(\\w+)" rule-str))
+  (let ((*readtable* (copy-readtable))
+        (s rule-str)
+        (start 0)
+        token)
+    (set-macro-character #\: (char-parser #\:))
+    (set-macro-character #\< (char-parser '<))
+    (set-macro-character #\> (char-parser '>))
+    (loop do (multiple-value-bind (obj pos) (read-from-string s nil nil :start start)
+               (setf start pos)
+               (setf token obj))
+          while token collect token)))
 
 (defun parse-workflow (workflow-str)
-  (ppcre:register-groups-bind ((#'intern workflow) rules) ("(\\w+){(.*)}" workflow-str)
+  (ppcre:register-groups-bind ((#'read-from-string workflow) rules) ("(\\w+){(.*)}" workflow-str)
     (cons workflow
           (mapcar #'parse-rule (ppcre:split "," rules)))))
 
 (defun parse-part (part-str)
   (loop for val-str in (ppcre:split "," (subseq part-str 1 (1- (length part-str))))
-        collect (cons (elt val-str 0) (parse-integer (subseq val-str 2 (length val-str))))))
+        collect (cons (read-from-string (subseq val-str 0 1)) (parse-integer (subseq val-str 2 (length val-str))))))
 
 (destructuring-bind (rules parts) (ppcre:split "\\n\\n" *input*)
   (defparameter *workflows* (mapcar #'parse-workflow (ppcre:split "\\n" rules)))
@@ -31,17 +46,21 @@
   (loop named rule-loop
         for rule in wf
         do (trivia:match rule
-               ((list sym-str "<" num-str ":" action)
-                (if (< (part-val part (elt sym-str 0)))
-                    ))
-             ((list )))))
+             ((list sym '< num #\: action)
+              (if (< (part-val part sym) num)
+                  (return-from rule-loop action)))
+             ((list sym '> num #\: action)
+                (if (> (part-val part sym) num)
+                    (return-from rule-loop action)))
+             ((list sym)
+              (return-from rule-loop sym)))))
 
 (defun handle-wf (part)
   (loop named wf-loop
-        for sym = '|in| then (match-rule part (alexandria:assoc-value *workflows* sym))
+        for sym = 'in then (match-rule part (alexandria:assoc-value *workflows* sym))
         do (case sym
-             ('A (return-from wf-loop (loop for v in '(|x| |m| |a| |s|) sum (part-val part v))))
-             ('R (return-from wf-loop 0)))))
+             (A (return-from wf-loop (loop for v in '(x m a s) sum (part-val part v))))
+             (R (return-from wf-loop 0)))))
 
 (defun part1 ()
   (loop for part in *parts*
