21ac45882b3ded8f3db32ccbfab3a68a974471d6
Emil Segerbäck <emil@segerback.ninja>
2024-12-02 13:32:45 +0100

    Solve 2b/2024

--- 2024/2.py
+++ 2024/2.py
@@ -4,10 +4,20 @@ lines = Path('2input').read_text().split('\n')
 
 parsed_lines = [[int(i) for i in l.split(' ')] for l in lines if l]
 
-safe_count = 0
-for l in parsed_lines:
+def is_safe(l):
     ds = [a-b for a,b in zip(l[1:], l[:-1])]
-    if (all(d < 0 for d in ds) or all(d > 0 for d in ds)) and all(abs(d) <= 3 for d in ds):
+    return (all(d < 0 for d in ds) or all(d > 0 for d in ds)) and all(abs(d) <= 3 for d in ds)
+
+safe_count = 0
+safe_count2 = 0
+for l in parsed_lines:
+    if is_safe(l):
         safe_count += 1
 
+    for i in range(len(l)):
+        if is_safe(l[:i] + l[i+1:]):
+            safe_count2 += 1
+            break
+
 print('Part 1:', safe_count)
+print('Part 2:', safe_count2)
