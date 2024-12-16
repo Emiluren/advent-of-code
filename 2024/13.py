fbea4cd56eed1ce0deb85b4136c295654899781e
Emil Segerbäck <emil@segerback.ninja>
2024-12-16 14:16:57 +0100

    Solve 13a/2024

--- 2024/13.py
+++ 2024/13.py
@@ -1,18 +1,27 @@
-import numpy as np
 from pathlib import Path
 
-machine_strs = Path('13input').read_text().split('\n\n')
+machine_strs = Path('13input').read_text().strip().split('\n\n')
 
 def split_ab(ab):
     x, y = ab.split(': ')[1][len('X+'):].split(', ')
     return int(x), int(y[2:])
 
-for m in machine_strs:
+def solution(m):
     a_str, b_str, p_str = m.split('\n')
     ax, ay = split_ab(a_str)
     bx, by = split_ab(b_str)
     px, py = split_ab(p_str)
 
-    print(repr(np.linalg.solve(np.array([[ax, bx], [ay, by]]), np.array([px, py]))))
-    break
+    for a in range(100):
+        for b in range(100):
+            if ax*a + bx*b == px and ay*a + by*b == py:
+                return a*3 + b
 
+tokens = 0
+
+for m in machine_strs:
+    sol = solution(m)
+    if sol:
+        tokens += sol
+
+print('Part 1:', tokens)
