2411abfc19f1a73b2b8836b56ff2595666f6a61a
Emil Segerbäck <emilsegerback@gmail.com>
2024-12-22 20:37:54 +0100

    Started working on 22b/2024

--- 2024/22.py
+++ 2024/22.py
@@ -8,9 +8,17 @@ def iterate(n):
     return ((n * 2048) ^ n) % 16777216
 
 res = 0
+changes = set()
 for n in nums:
+    diffs = []
     for i in range(2000):
         n = iterate(n)
+        if i > 0:
+            diffs.append((n % 10) - last_n)
+        last_n = n % 10
     res += n
+
+    for i in range(2000-4):
+        changes.add(tuple(diffs[i:i+4]))
 
 print('Part 1:', res)
