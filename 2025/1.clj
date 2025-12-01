7288b86426984bed99fc711dd28ae75679c38243
Emil Segerbäck <emil@segerback.ninja>
2025-12-01 11:19:44 +0100

    Solve 1b/2025

--- 2025/1.clj
+++ 2025/1.clj
@@ -12,3 +12,25 @@
   (reductions #(mod (+ %1 %2) 100) 50 *decoded-input*))
 
 (println "Part 1:" (get (frequencies *all-states*) 0))
+
+;; Could probably be massively simplified using modular arithmetic
+(defn passes-of-0 [start n]
+  (loop [pos start
+         count-0 0
+         i 0]
+    (if (= (abs n) i)
+      [pos count-0]
+      (let [new-pos (mod (if (< n 0)
+                           (- pos 1)
+                           (+ pos 1))
+                         100)]
+        (recur new-pos
+               (if (= 0 new-pos)
+                 (+ count-0 1)
+                 count-0)
+               (+ i 1))))))
+
+(println "Part 2:"
+         (->> (map passes-of-0 *all-states* *decoded-input*)
+              (map second)
+              (reduce +)))
