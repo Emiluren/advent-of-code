e4bb213fc0ebc865ae1ec6bff30f8af9f3a6df6c
Emil Segerbäck <emilsegerback@mykolab.ch>
2025-12-06 08:38:48 +0100

    Solve 6b/2025

--- 2025/6.clj
+++ 2025/6.clj
@@ -6,7 +6,7 @@
   6 98  215 314
 *   +   *   +  ")
 
-(defn process-input [input-str]
+(defn process-input-p1 [input-str]
   (->> (for [l (str/split-lines input-str)]
          (str/split (str/trim l) #"\s+"))
        (apply map vector)
@@ -15,6 +15,22 @@
                    (map parse-long (rest %))))))
 
 (println "Part 1:"
-         (->> (process-input (slurp "6input"))
+         (->> (process-input-p1 (slurp "6input"))
               (map #(apply (first %) (rest %)))
+              (reduce +)))
+
+(defn process-input-p2 [input-str]
+  (let [cols (->> (apply map str (str/split-lines input-str))
+                  (partition-by #(re-matches #"\s+" %))
+                  (filter #(not (and (= 1 (count %))
+                                     (re-matches #"\s+" (first %))))))]
+    (for [c cols
+          :let [op (last (first c))
+                first-num (apply str (butlast (first c)))
+                num-strs (cons first-num (rest c))]]
+      (cons op (map #(parse-long (str/trim %)) num-strs)))))
+
+(println "Part 2:"
+         (->> (process-input-p2 (slurp "6input"))
+              (map #(apply ({\* *, \+ +} (first %)) (rest %)))
               (reduce +)))
