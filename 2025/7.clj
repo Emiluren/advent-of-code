055093e7dba9a5152fe989e7802e48182c2abd15
Emil Segerbäck <emilsegerback@mykolab.ch>
2025-12-07 13:15:32 +0100

    Attempt to solve 7b/2025

--- 2025/7.clj
+++ 2025/7.clj
@@ -6,19 +6,33 @@
     (set (keep-indexed #(if (not (= \. %2)) %1) l))))
 
 (defn split-beams [beams-to-split]
-  (reduce set/union
-          (map (fn [b] #{(- b 1) (+ b 1)})
-               beams-to-split)))
+  (apply merge-with +
+         (map (fn [[index amount]] {(- index 1) amount
+                                    (+ index 1) amount})
+              beams-to-split)))
+
+(defn simulate-beam-states [splitter-lines]
+  (reductions (fn [beams splitters]
+                (let [beams-to-split (select-keys beams splitters)]
+                  (merge (apply dissoc beams splitters)
+                         (split-beams beams-to-split))))
+              {(first (first splitter-lines)) 1}
+              (rest splitter-lines)))
+
+(defn part1 [splitter-lines beam-states]
+  (->> (map (fn [beams splitters]
+                     (count (select-keys beams splitters)))
+                   (butlast beam-states)
+                   (rest splitter-lines))
+              (reduce +)))
+
+(def splitter-lines (process-input (slurp "7input")))
+(def beam-states (simulate-beam-states splitter-lines))
 
 (println "Part 1:"
-         (let [splitter-lines (process-input (slurp "7input"))
-               beam-states (reductions (fn [beams splitters]
-                                         (let [beams-to-split (set/intersection beams splitters)]
-                                           (set/union (set/difference beams beams-to-split)
-                                                      (split-beams beams-to-split))))
-                                       splitter-lines)]
-           (->> (map (fn [beams splitters]
-                       (count (set/intersection beams splitters)))
-                     (butlast beam-states)
-                     (rest splitter-lines))
-                (reduce +))))
+         (part1 splitter-lines beam-states))
+
+; 10434635933 too low
+; 25 instead of 40 for testinput
+(println "Part 2:"
+         (reduce + (vals (last (simulate-beam-states (process-input (slurp "7testinput")))))))
