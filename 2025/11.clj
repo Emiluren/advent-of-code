095f5ec4bd88c2c5a47c1f104aa48e23200119f3
Emil Segerbäck <emilsegerback@gmail.com>
2025-12-15 19:37:43 +0100

    Solve 11b/2025

--- 2025/11.clj
+++ 2025/11.clj
@@ -7,28 +7,34 @@
                                       (mapv keyword))}))
        (reduce merge)))
 
-(def input-map (process-input (slurp "10input")))
+(def input-map (process-input (slurp "11input")))
 
 (defmacro defn-memo [name args & body]
   `(def ~name
      (memoize (fn ~args
                 ~@body))))
 
-(defn get-paths [from]
-  (if (= :out from)
-    [[:out]]
+(defn-memo get-paths [from to]
+  (if (= to from)
+    [[to]]
     (for [output (get input-map from)
-          sub-path (get-paths output)]
-      (cons from sub-path))))
+          sub-path (get-paths output to)]
+      (lazy-seq (cons from sub-path)))))
 
 (println "Part 1:"
-         (count (get-paths :you)))
+         (count (get-paths :you :out)))
 
-(defn count-paths [from]
-  (if (= :out from)
+(defn-memo count-paths [from to]
+  (if (= from to)
     1
-    (for [output (get input-map from)
-          sub-path (get-paths output)]
-      (cons from sub-path))))
+    (->> (for [output (get input-map from)]
+           (count-paths output to))
+         (reduce +))))
 
-;;(def paths2 (get-paths :svr))
+(println "Part 2:"
+         (+ (* (count-paths :svr :fft)
+               (count-paths :fft :dac)
+               (count-paths :dac :out))
+            (* (count-paths :svr :dac)
+               (count-paths :dac :fft)
+               (count-paths :fft :out))))
